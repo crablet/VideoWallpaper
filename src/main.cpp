1042013b6c2897670673efcddceb5be98f8afd8d
crablet <love4uandgzfc@gmail.com>
2020-02-24 13:04:12 +0800

    新增禁止多开功能
    因为多开时多个视频会同时出现在桌面上，体验不好

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,19 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    auto mutexHandle = CreateMutexA(nullptr, false, "VideoWallpaper_Mutex");
+    if (!mutexHandle || GetLastError() == ERROR_ALREADY_EXISTS)    // 防止进程多开
+    {
+        CloseHandle(mutexHandle);   // 其实不写也没事，后面else为了代码好看点就没写
+
+        return 0;
+    }
+    else
+    {
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.show();
+
+        return a.exec();    // 无需手动CloseHandle()，因为操作系统会自动回收mutexHandle
+    }
 }
